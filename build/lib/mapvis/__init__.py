45faf7506befe5e48c1b15af6bf96995eb070be3
Maximilian Schumacher <schummax@icloud.com>
2025-05-29 21:02:14 +0200

    remove .DS_Store, build/, and mapvis.egg-info/ from repo

--- build/lib/mapvis/__init__.py
+++
@@ -1,7 +0,0 @@
-"""MapVis: A module for visualizing celltype and feature mappings."""
-
-from .core import MapVisualizer
-from .utils import load_mapping_from_csv, load_mapping_from_tsv
-
-__version__ = "0.1.0"
-__all__ = ["MapVisualizer", "load_mapping_from_csv", "load_mapping_from_tsv"]
